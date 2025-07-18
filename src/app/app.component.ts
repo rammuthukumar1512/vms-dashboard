fd8cc89f2267359614759feb17dd2d771c67d799
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-07-18 16:14:05 +0530

    resolve merge conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';  // <-- add this import
 import { ComputerDashboardComponent } from './computer-dashboard/computer-dashboard.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-=======
-  standalone: true,
->>>>>>> 219decf0d3acfff4cd3d7536bcad00b1f40d32fb
   imports: [RouterOutlet, ComputerDashboardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // <-- note: it's `styleUrls`, not `styleUrl`
