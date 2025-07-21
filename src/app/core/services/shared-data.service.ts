05ef66e59f8426ec310935d3fb871fe42b4e8965
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-07-21 10:29:28 +0530

    chart alignment completed

--- src/app/core/services/shared-data.service.ts
+++ src/app/core/services/shared-data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { ApplicationDetails } from '../../models/computer.model';
 
 @Injectable({
   providedIn: 'root'
