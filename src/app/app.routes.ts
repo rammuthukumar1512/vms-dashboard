5bf29a8e36492bbba4bf1c0945c5255a885bdfc9
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-07-29 10:53:17 +0530

    set routing setup with fallback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path:'', loadComponent: () => import('./main-dashboard/main-dashboard.component').then(m=> m.MainDashboardComponent),
-        children: [{
+    {
+        path:'', loadComponent: () => import('./main-dashboard/main-dashboard.component').then(m=> m.MainDashboardComponent),
+        children: [{ path: '', redirectTo: 'computer-overview', pathMatch: 'full' } ,{
             path: 'computer-overview',
             loadComponent:() => import('./computer-dashboard/computer-dashboard.component').then(m => m.ComputerDashboardComponent)
-        }]
-    }
+        }
+    ]
+    },
+    {path: '**', redirectTo: ''}
 ];
