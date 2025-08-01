b3bb6bfbb4a06de5b24090dbc8a841d7bd4cac30
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-08-01 16:28:41 +0530

    add environmwnts

---
+++ src/environments/environments.ts
@@ -0,0 +1,9 @@
+export const environments = {
+  production: false,
+  unique_url: 'http://localhost:8082/api/getDashboardMetrics',
+  likelyCpeUrl: 'http://localhost:8082/api/hint/likelyCpeNames',
+  addHintUrl: 'http://localhost:8082/api/hint/addHint',
+  unresolvedAppsUrl: 'http://localhost:8082/api/applications/unresolved' ,
+  searchVulnerabilityUrl: 'http://localhost:8082/api/search/vulnerability'
+
+};
