a705b02c96812b2ff82a0e7601c1df6f386a6648
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-08-11 22:13:09 +0530

    pagination mobile view completed

--- src/app/core/services/shared-data.service.ts
+++ src/app/core/services/shared-data.service.ts
@@ -6,10 +6,16 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class SharedDataService {
   private appDataSubject = new BehaviorSubject<any>(null);
+  private isMobileSubject = new BehaviorSubject<boolean>(false);
 
   currentData$ = this.appDataSubject.asObservable();
+  isMobile$ = this.isMobileSubject.asObservable();
 
   sendAppData(data: any): void {
     this.appDataSubject.next(data);
+  }
+
+  setMobile(isMobile: boolean): void {
+    this.isMobileSubject.next(isMobile);
   }
 }
