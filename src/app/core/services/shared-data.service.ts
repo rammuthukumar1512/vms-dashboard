4035e45d49496d79c1d0bfeb2c9200d5ff6564f2
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-10-22 12:14:11 +0530

    Revert "Merge pull request #116 from rammuthukumar1512/vms-muthu-new"
    This reverts commit a4e5c044597e6db03f9d534fab57de83ccbca0e3, reversing
    changes made to b5c61b9be1791499de6e1432192e1838988c0fa2.

--- src/app/core/services/shared-data.service.ts
+++ src/app/core/services/shared-data.service.ts
@@ -1,19 +1,13 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedDataService {
   private appDataSubject = new BehaviorSubject<any>(null);
+
   currentData$ = this.appDataSubject.asObservable();
-
-  private syncSecurityDataSubject = new Subject<void>();
-  syncSecurityData$ = this.syncSecurityDataSubject.asObservable();
-
-  syncSecurityData(): void {
-    this.syncSecurityDataSubject.next();
-  }
 
   sendAppData(data: any): void {
     this.appDataSubject.next(data);
--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MatNavList } from '@angular/material/list';
 import { MatSidenav, MatSidenavContent, MatSidenavModule} from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
@@ -9,41 +9,28 @@ import { RouterLink } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { ComputerDashboardComponent } from '../computer-dashboard/computer-dashboard.component';
-import { SharedDataService } from '../core/services/shared-data.service';
-import { AppRoutes } from '../../environments/approutes';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-main-dashboard',
   imports: [CommonModule, MatIconModule, MatSidenavModule, MatToolbar, MatNavList,
-          RouterOutlet, RouterLink, MatTooltipModule, RouterModule, ComputerDashboardComponent],
+          RouterOutlet, RouterLink, MatTooltipModule, RouterModule],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.css'
 })
-export class MainDashboardComponent implements OnInit, AfterViewInit, AfterViewChecked{
+export class MainDashboardComponent implements AfterViewInit, AfterViewChecked{
     isMiniSidenav = false;
     isMobile = false;
     sidenavPosition!: 'start';
     hoverState: String = "";
-    urlMatch: boolean = false;
 
     @ViewChild('sidenav') sidenav!: MatSidenav;
     @ViewChild('matSideNavContent') matSideNavContent!: MatSidenavContent;
-    @ViewChild('computerDashboard') computerDashboard!: ComputerDashboardComponent;
     sideMenuItems = [ {title: 'Computer Overview', icon: 'dashboard', link: '/computer-overview'},
     {title: 'Resolve Applications', icon: 'app_registration', link: '/resolve-applications'},
     {title: 'Search Vulnerability', icon: 'search_insights', link: '/cpe-cve-search'}];
     
-  constructor(private breakpointObserver: BreakpointObserver, private sharedDataService: SharedDataService, private router: Router){}
-   ngOnInit(): void {
-        let currentUrl = this.router.url;
-        this.urlMatch = currentUrl ? !currentUrl.match(AppRoutes.cpe_cve_search) : false;
-     this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
-        currentUrl = event.urlAfterRedirects;
-        this.urlMatch = currentUrl ? !currentUrl.match(AppRoutes.cpe_cve_search) : false;
-     });
-   }
+    constructor(private breakpointObserver: BreakpointObserver){}
+
   ngAfterViewInit(): void {
     this.setupSidenav();
   }
@@ -56,7 +43,6 @@ export class MainDashboardComponent implements OnInit, AfterViewInit, AfterViewC
 
   private setupSidenav(): void {
     if (!this.sidenav) {
-      console.error('Sidenav not found');
       return;
     }
 
@@ -66,7 +52,6 @@ export class MainDashboardComponent implements OnInit, AfterViewInit, AfterViewC
     this.breakpointObserver
       .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
       .subscribe(result => {
-        console.log(result);
         this.isMobile = result.matches;
         this.sidenav.mode = this.isMobile ? 'over' : 'side';
         this.sidenav.opened = !this.isMobile;
@@ -75,7 +60,6 @@ export class MainDashboardComponent implements OnInit, AfterViewInit, AfterViewC
   }
 
    toggleSidenavMode(): void {
-    console.log(this.isMobile)
     if (this.isMobile) {
       this.sidenav.toggle();
     }
@@ -92,10 +76,6 @@ public onMouseLeave() {
        this.hoverState = "";
      }, 300);
    } 
-}
-
-public syncSecurityData() {
-    this.sharedDataService.syncSecurityData();
 }
 
 }
